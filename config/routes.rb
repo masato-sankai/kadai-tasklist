bcabe6f651982b1988b0a5e1179a9f1a4428e6a7
masato-sankai <taro.kirameki@techacademy.jp>
2018-06-17 12:01:10 +0000

    kadai-tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
-  
+  get 'tasks/:id', to: 'tasks#show'
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create, :update]
 
-  resources :tasks, only: [:create, :destroy, :new]
+  resources :tasks, only: [:create, :destroy, :new, :edit, :update]
 end
 
