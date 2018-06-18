3fb155df52bb9a44e6dca489457235af6e9ada80
masato-sankai <taro.kirameki@techacademy.jp>
2018-06-18 09:28:33 +0000

    kadai-tasklist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = LoginUser.find(params[:id])
     @tasks = @user.tasks.order('created_at DESC').page(params[:page])
     counts(@user)
   end
